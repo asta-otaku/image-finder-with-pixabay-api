c8b9929971e15e1b52f16951a61f82c54a6bc70d
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2022-09-07 13:30:29 +0100

    fix: Added pop view to the images rendered

--- src/components/ImageResults.js
+++ src/components/ImageResults.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 function ImageResults(props) {
   let imageListContent;
   const { images } = props;
+  const [popView, setPopView] = useState(false);
+  const [currentImage, setCurrentImage] = useState(null);
 
   if (images) {
     imageListContent = (
       <div className="card-container">
         {images.map((img) => (
-          <div className="image-card" key={img.id}>
+          <div
+            className="image-card"
+            key={img.id}
+            onClick={() => {
+              setCurrentImage(img.largeImageURL);
+              setPopView(true);
+            }}
+          >
             <img src={img.largeImageURL} alt="" />
             <div className="image-details">
               {img.tags}{" "}
@@ -25,7 +34,17 @@ function ImageResults(props) {
     imageListContent = null;
   }
 
-  return <div>{imageListContent}</div>;
+  return (
+    <>
+      {popView ? (
+        <div className="pop-header" onClick={() => setPopView(false)}>
+          <img src={`${currentImage}`} alt="" />
+        </div>
+      ) : (
+        <div>{imageListContent}</div>
+      )}
+    </>
+  );
 }
 
 ImageResults.propTypes = {
